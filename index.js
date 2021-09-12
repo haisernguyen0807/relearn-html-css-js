0025290b32891a8f157b15c5bdf8a65f37e62a36
Nguyễn Ngọc Hoàng <77830327+haisernguyen0807@users.noreply.github.com>
2021-09-12 12:49:56 +0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#mobile_menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+//display mobile menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu)
